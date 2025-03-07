3c81402462e6be5894412d380818e6b27785e114
onik <onikahmedonikahmed@gmail.com>
2025-03-06 21:21:32 -0800

    simple text

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("hello World")
+import streamlit as st
+
+#
+st.set_page_config(page_title="Website App", page_icon=":tada:",layout="wide")
+with st.container():
+    st.subheader("Hi, this is Onik :wave:")
+    st.title("Computer scientist from Los Angeles")
+    st.write("Coding sucks!!")
+    st.write("[LinkedIn>](https://www.linkedin.com/in/onikahmed)")
+
