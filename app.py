d7cfe538a65afb3cdc56cbcb1a2825c85667eb6d
onik <onikahmedonikahmed@gmail.com>
2025-03-07 01:40:58 -0800

    added a button

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.set_page_config(page_title="Website App", page_icon=":tada:",layout="wide")
 with st.container():
     st.subheader("Hi, this is Onik :wave:")
     st.title("Computer science student")
-    st.write("Coding sucks!!!!")
+    st.write("Coding id fun!?")
     st.write("[LinkedIn>](https://www.linkedin.com/in/onikahmed)")
 with st.container():
     st.write("---")
@@ -13,10 +13,16 @@ with st.container():
     with left_column:
         st.header("Column 1")
         st.write("##")
-        st.write("Python")
+        st.write("This is a Test")
     with right_column:
         st.header("Column 2")
     with middle_column:
         st.header("Column 3")
     with middle1_column:
         st.header("Column 4")
+        clicked=st.button("Hi")
+        if clicked:
+            st.write("hello")
+
+with st.container():
+    st.write("---")
