564c653ffc33ef23270a86aa08276351022321af
onik <onikahmedonikahmed@gmail.com>
2025-03-06 21:25:35 -0800

    simple text

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ st.set_page_config(page_title="Website App", page_icon=":tada:",layout="wide")
 with st.container():
     st.subheader("Hi, this is Onik :wave:")
     st.title("Computer scientist from Los Angeles")
-    st.write("Coding sucks!!")
+    st.write("Coding sucks!!!")
     st.write("[LinkedIn>](https://www.linkedin.com/in/onikahmed)")
 
