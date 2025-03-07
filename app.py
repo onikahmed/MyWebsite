83c30760371ac1727d90442bea1f9e626276bfb3
onik <onikahmedonikahmed@gmail.com>
2025-03-07 00:28:31 -0800

    updated sentences

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import streamlit as st
 st.set_page_config(page_title="Website App", page_icon=":tada:",layout="wide")
 with st.container():
     st.subheader("Hi, this is Onik :wave:")
-    st.title("Computer scientist from Los Angeles")
+    st.title("Computer science student")
     st.write("Coding sucks!!!!")
     st.write("[LinkedIn>](https://www.linkedin.com/in/onikahmed)")
 with st.container():
