12da15fb28e88953914f303f2b0a656ae3b666c9
onik-ahmed <onikahmedonikahmed@gmail.com>
2025-05-29 21:26:08 -0700

    about page also added

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
 
 def home(request):
     return render(request, 'blog/home.html')
 
 def about(request):
-    return HttpResponse('<h1>Blog About<h1>')
+    return render(request, 'blog/about.html')
