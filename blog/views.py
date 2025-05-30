30637b00ab4f27c85ac434022290339d370731e0
onik-ahmed <onikahmedonikahmed@gmail.com>
2025-05-29 21:35:52 -0700

    stopped at 12:31 part 3

--- blog/views.py
+++ blog/views.py
@@ -21,7 +21,7 @@ def home(request):
     context = {
         'posts':posts
     }
-    return render(request, 'blog/home.html')
+    return render(request, 'blog/home.html',context)
 
 def about(request):
     return render(request, 'blog/about.html')
